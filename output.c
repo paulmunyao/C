d432d608fb84baacf4485268ad53ad05e2c98eaa
paulmunyao <paulmunyao094@gmail.com>
2023-03-06 11:39:53 +0300

    modify code

--- output.c
+++ output.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-#include<cs50.h>
 
 int main(){
-    string first = get_string("First name");
-    string last = get_string("Last name");
+    printf("2 cups of sugar\n");
+    printf("1 cups of sugar");
+    printf("\n2/3 cups of sugar");
     return 0;
 }
