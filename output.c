26419afd701e5f72278c31726882ff33aa22e0c6
paulmunyao <paulmunyao094@gmail.com>
2023-03-06 11:42:01 +0300

    comment out unused code

--- output.c
+++ output.c
@@ -6,3 +6,12 @@ int main(){
     printf("\n2/3 cups of sugar");
     return 0;
 }
+
+// #include<stdio.h>
+// #include<cs50.h>
+
+// int main(){
+//     string first = get_string("First name?");
+//     string last = get_string("Last name");
+//     return 0;
+// }
