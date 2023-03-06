276946472cc23fc3a329766924657fc54d41ebda
paulmunyao <paulmunyao094@gmail.com>
2023-03-06 19:38:53 +0300

    initialize trunk in c

---
+++ getName.c
@@ -0,0 +1,8 @@
+// #include<stdio.h>
+// #include<cs50.h>
+
+// int main(){
+//     string first = get_string("First name?");
+//     string last = get_string("Last name");
+//     return 0;
+// }
--- output.c
+++ output.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
-
-int main(){
-    printf("2 cups of sugar\n");
-    printf("1 cups of sugar");
-    printf("\n2/3 cups of sugar");
-    return 0;
-}
-
 // #include<stdio.h>
-// #include<cs50.h>
 
 // int main(){
-//     string first = get_string("First name?");
-//     string last = get_string("Last name");
+//     printf("2 cups of sugar\n");
+//     printf("1 cups of sugar");
+//     printf("\n2/3 cups of sugar");
 //     return 0;
 // }
+
+#include<stdio.h>
+#include<cs50.h>
+
+int main(){
+    string first = get_string("First name?");
+    string last = get_string("Last name");
+    return 0;
+}
