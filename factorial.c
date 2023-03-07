b099bc9dd676c1040a82bdaf7105f935818d29bb
paulmunyao <paulmunyao094@gmail.com>
2023-03-07 15:59:30 +0300

    update factorial file

--- factorial.c
+++ factorial.c
@@ -1,22 +1,22 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n, factorial = 1;
+int main() {
+    int n, factorial = 1;
 
-//     printf("Enter a number: ");
-//     scanf("%d", &n);
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-//     if (n < 0) {
-//         printf("Error: Factorial of negative numbers does not exist.");
-//         return 0;
-//     }
+    if (n < 0) {
+        printf("Error: Factorial of negative numbers does not exist.");
+        return 0;
+    }
 
-//     for (int i = 1; i <= n; i++) {
-//         factorial *= i;
-//     }
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
 
-//     printf("%d! = %d", n, factorial);
+    printf("%d! = %d", n, factorial);
 
-//     return 0;
-// }
+    return 0;
+}
 
